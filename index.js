d9613e89efd8a5c6b729414d3b21fc5d5d091269
Abián Izquierdo <abizmo@gmail.com>
2022-08-12 20:35:14 +0100

    Close menu when click other place than button

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const primaryHeader = document.querySelector('.primary-header');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNavigation = document.querySelector('.primary-navigation');
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener('click', (evt) => {
+  evt.stopPropagation();
   primaryNavigation.hasAttribute('data-visible')
     ? navToggle.setAttribute('aria-expanded', 'false')
     : navToggle.setAttribute('aria-expanded', 'true');
   primaryNavigation.toggleAttribute('data-visible');
   primaryHeader.toggleAttribute('data-overlay');
+});
+
+document.addEventListener('click', () => {
+  if (primaryNavigation.hasAttribute('data-visible')) {
+    navToggle.setAttribute('aria-expanded', 'false');
+    primaryNavigation.removeAttribute('data-visible');
+    primaryHeader.removeAttribute('data-overlay');
+  }
 });
